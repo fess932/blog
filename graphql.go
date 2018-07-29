1b852c7d4b8c4a4d4d17b8e23c1a3c74ce7f85a7
FESS-PC\User <fess932@gmail.com>
2018-07-29 17:12:38 +0300

    d

--- graphql.go
+++ graphql.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
-	$END$
+// Article is one path of blog
+type Article struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+	Text string `json:"text"`
 }
--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
 
 func main() {
-	
+	p := `:3000`
+	a := `127.0.0.1`
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("its login"))
+	})
+	r.Get("/blog", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("its blog"))
+	})
+	r.Get("/main", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("its main"))
+	})
+	fmt.Printf("Сервер слушает на адресе %v%v", a, p)
+	http.ListenAndServe(p, r)
 }
